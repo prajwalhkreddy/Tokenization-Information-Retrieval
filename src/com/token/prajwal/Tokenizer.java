5432ddd250953f9a58b9ca911e15409602124140
Prajwal <prajwal19@gmail.com>
2016-02-16 15:50:38 -0600

    working hashmap

--- src/com/token/prajwal/Tokenizer.java
+++ src/com/token/prajwal/Tokenizer.java
@@ -2,6 +2,7 @@ package com.token.prajwal;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,25 +10,37 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Tokenizer {
-	final static File folder = new File("Cranfield");
+	final static File folder = new File("Prajwal");
 	public static List<String> fileNames = new ArrayList<String>();
-	public static Map<String,Integer> countMap = new HashMap<String,Integer>();
+	public static Map<String, Integer> countMap = new HashMap<String, Integer>();
 
 	public static void main(String[] args) {
 		listFilesForFolder(folder);
-		System.out.println();
+		fileReader();
+		System.out.println("Map:"+countMap);
 	}
-
-	public void fileReader(String filePath) {
+	
+	@SuppressWarnings("resource")
+	public static void fileReader() {
 
 		try {
 			for (int i = 0; i < fileNames.size(); i++) {
 
-				String tempPath = "Cranfield\\" + fileNames.get(i);
+				String tempPath = folder + "\\" + fileNames.get(i);
 
+				
 				Scanner readFile = new Scanner(new File(tempPath))
 						.useDelimiter("[^a-zA-Z0-9]+");
-				
+
+				while (readFile.hasNext()) {
+					String word = readFile.next().toLowerCase();
+
+					if (countMap.containsKey(word)) {
+						countMap.put(word, countMap.get(word) + 1);
+					} else {
+						countMap.put(word, 1);
+					}
+				}
 
 			}
 		} catch (FileNotFoundException e) {
